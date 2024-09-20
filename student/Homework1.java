781288be4cf98bd9889c5852c0b975523840a4ad
Ruxshona Baymanova <147691637+rbaymanova@users.noreply.github.com>
2024-09-20 17:46:17 +0500

    Update Homework1.java

--- student/Homework1.java
+++ student/Homework1.java
@@ -1,76 +1,94 @@
-   package student;
+package student;
 
 public class Homework1 {
 
-    public double shippingCalculator(int n){
-        //TODO Write your code here for problem 1
-        if (n==1){
-            System.out.println("10.95$");
-        }else if (n>=1){
-            System.out.println(10.95+(2.95*(n-1)) + "$");
-        }else System.out.println("Enter positive number.");
-        return 0;
-    }
-    public boolean isValidTriangle(int a, int b, int c){
-        if (a+b>c && b+c>a && c+a>b) {
-            return true;
+    // Shipping calculator
+    public double shippingCalculator(int n) {
+        // Return the calculated value instead of printing
+        if (n == 1) {
+            return 10.95;
+        } else if (n > 1) {
+            return 10.95 + (2.95 * (n - 1));
+        } else {
+            System.out.println("Enter a positive number.");
+            return 0;
         }
-        else {
+    }
+
+    // Check if a triangle is valid
+    public boolean isValidTriangle(int a, int b, int c) {
+        return a + b > c && b + c > a && c + a > b;
+    }
+
+    // Prime number checker
+    public boolean isPrime(int number) {
+        if (number <= 1) {
             return false;
         }
-    }
-    public boolean isPrime(int number){
-        //TODO Write your code here for problem 3
-        Boolean isPrime = true;
-        if(items<1){
-            isPrime = false;
-        }
-        for(int i=2; i<=Math.sqrt(items); i++){
-            if(items%i==0){
-                isPrime = false;
-                break;
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
             }
         }
-        return isPrime;
+        return true;
     }
 
-   public  int hex2int(String hex) {
-    //TODO Write your code here for problem 4
-    if(num.length()>1)
-        return -1;
-    if(Character.isDigit(num.charAt(0)))
-        return Integer.valueOf(num);
-    if(num.charAt(0)=='A'||num.charAt(0)=='a')
-        return 10;
-    if(num.charAt(0)=='B'||num.charAt(0)=='b')
-        return 11;
-    if(num.charAt(0)=='C'||num.charAt(0)=='c')
-        return 12;
-    if(num.charAt(0)=='D'||num.charAt(0)=='d')
-        return 13;
-    if(num.charAt(0)=='E'||num.charAt(0)=='e')
-        return 14;
-    if(num.charAt(0)=='F'||num.charAt(0)=='f')
-        return 15;
-    return -1;
-}
-public String int2hex(int value) {
-    if(num<0||num>15)
-        return "-1";
-    if(num<10)
-        return String.valueOf(num);
-    if(num==10)
-        return "A";
-    if(num==11)
-        return "B";
-    if(num==12)
-        return "C";
-    if(num==13)
-        return "D";
-    if(num==14)
-        return "E";
-    return "F";
-}
+    // Hexadecimal to integer converter
+    public int hex2int(String hex) {
+        if (hex.length() > 1) {
+            return -1;
+        }
+        char ch = hex.charAt(0);
+        if (Character.isDigit(ch)) {
+            return Integer.parseInt(hex);
+        }
+        switch (ch) {
+            case 'A':
+            case 'a':
+                return 10;
+            case 'B':
+            case 'b':
+                return 11;
+            case 'C':
+            case 'c':
+                return 12;
+            case 'D':
+            case 'd':
+                return 13;
+            case 'E':
+            case 'e':
+                return 14;
+            case 'F':
+            case 'f':
+                return 15;
+            default:
+                return -1;
+        }
+    }
 
-
+    // Integer to hexadecimal converter
+    public String int2hex(int value) {
+        if (value < 0 || value > 15) {
+            return "-1";
+        }
+        if (value < 10) {
+            return String.valueOf(value);
+        }
+        switch (value) {
+            case 10:
+                return "A";
+            case 11:
+                return "B";
+            case 12:
+                return "C";
+            case 13:
+                return "D";
+            case 14:
+                return "E";
+            case 15:
+                return "F";
+            default:
+                return "-1";
+        }
+    }
 }
